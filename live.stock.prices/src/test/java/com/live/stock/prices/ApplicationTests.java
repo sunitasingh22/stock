d0441b8633d7495ed8b0ad0afa1295dd40a0a36e
Sunita Singh <sona222000@gmail.com>
2024-10-28 23:15:59 +0000

    first commit

---
+++ live.stock.prices/src/test/java/com/live/stock/prices/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.live.stock.prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
